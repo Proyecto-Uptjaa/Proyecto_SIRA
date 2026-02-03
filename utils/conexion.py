20aaace6623a231f70d9c729effbe61a9cfe8789
jorged-97 <jorgej1@proton.me>
2026-02-02 21:42:10 -0400

    fix: Error de conexion en Except

--- utils/conexion.py
+++ utils/conexion.py
@@ -1,16 +1,18 @@
-
-from utils.db import get_connection  # tu función central de conexión
+from utils.db import get_connection
 
 def verificar_conexion_bd() -> bool:
+    """Verifica si la conexión a la BD está disponible."""
     conexion = None
     cursor = None
     try:
         conexion = get_connection()
+        if not conexion:
+            return False
         cursor = conexion.cursor()
         cursor.execute("SELECT 1")
         cursor.fetchone()
         return True
-    except get_connection().Error as err:
+    except Exception as err:
         print(f"Error de conexión a la BD: {err}")
         return False
     finally:
